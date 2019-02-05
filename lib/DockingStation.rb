be03f8a1f9e91aedf64207db8f4e64456fc00cae
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-02-05 16:43:16 +0000

    write test when a bike is returned

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -2,4 +2,6 @@ class DockingStation
   def release_bike
     Bike.new
   end
+  def return_bike(bike)
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,6 +6,10 @@ describe DockingStation do
     expect(docking_station.release_bike.working?).to eq true
   end
 
+  it "docks a bike when returned" do
+    expect(docking_station.return_bike(Bike.new)).to eq nil
+  end
+
   #"releases a bike" do
   #   DockingStation.new.release_bike.should == true
   # end
