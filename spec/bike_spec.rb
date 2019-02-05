448a3c4b76ed3c1b3b9861a154ed7f72818e935a
Steve Kahn <steve@kahn.org.uk>
2019-02-05 16:25:46 +0000

    made the test check if the bike is working

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,5 +2,6 @@ require 'Bike.rb'
 
 describe Bike do
   subject { Bike.new.working? }
+  it { should eq true }
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,10 @@
 require "DockingStation.rb"
-
+require "Bike.rb"
 describe DockingStation do
-  subject { DockingStation.new.release_bike }
-  it { should eq nil }
+  docking_station = DockingStation.new
+  it "releases bike and expects it to work"  do
+    expect(docking_station.release_bike.working?).to eq true
+  end
 
   #"releases a bike" do
   #   DockingStation.new.release_bike.should == true
