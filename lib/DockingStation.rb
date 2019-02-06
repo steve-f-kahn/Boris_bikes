f1f147e08b832a0063307069647cf2aa2862b25a
Steve Kahn <steve@kahn.org.uk>
2019-02-06 09:30:23 +0000

    Added and passed test for returning bike

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,7 +1,19 @@
+require "./lib/Bike.rb"
 class DockingStation
-  def release_bike
-    Bike.new
+  def initialize(no_of_bikes = 1)
+    @docking_station = []
+    while @docking_station.length < no_of_bikes
+      @docking_station << Bike.new
+    end
+    @docking_station
+  end
+  def release_bike(x = 1)
+    bike = @docking_station[x-1]
+    @docking_station[x-1] = nil
+    bike
   end
   def return_bike(bike)
+    @docking_station[@docking_station.index(nil)] = bike
+    true
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,14 @@
 require "DockingStation.rb"
 require "Bike.rb"
 describe DockingStation do
-  docking_station = DockingStation.new
+  docking_station = DockingStation.new(2)
   it "releases bike and expects it to work"  do
-    expect(docking_station.release_bike.working?).to eq true
+    expect(docking_station.release_bike(1).working?).to eq true
   end
-
-  it "docks a bike when returned" do
-    expect(docking_station.return_bike(Bike.new)).to eq nil
+  bike = docking_station.release_bike(2)
+  it "returns a bike to the statiom"  do
+    expect(docking_station.return_bike(bike)).to eq true
   end
-
   #"releases a bike" do
   #   DockingStation.new.release_bike.should == true
   # end
